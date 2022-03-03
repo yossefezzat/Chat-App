f703e860f2ec8e2a988a987ea162cdd46f397c8e
yossefezzat <45318576+yossefezzat@users.noreply.github.com>
2022-03-03 12:32:00 +0200

    add location navigator

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,3 +1,4 @@
+
 const socket = io();
 
 socket.on('message', (message)=>{
@@ -8,5 +9,16 @@ document.querySelector('#message-form').addEventListener('submit', (e)=>{
     e.preventDefault();
     const message = e.target.elements.message.value;
     socket.emit('sendMessage',message);
+});
+
+document.querySelector("#location").addEventListener('click', (e)=> {
+    e.preventDefault()
+    if(!navigator.geolocation) return alert('unable to share your location');
+    navigator.geolocation.getCurrentPosition((position)=> {
+        socket.emit('location', {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+        })
+    });
 })
 
--- src/app.js
+++ src/app.js
@@ -16,9 +16,20 @@ io.on('connection', (socket)=>{
     console.log('welcome !');
 
     socket.emit('message', 'welcome!');
+    socket.broadcast.emit('message', 'new User has Joined');
+
     socket.on('sendMessage', (message) => {
         io.emit('message', message);
     });
+    
+    socket.on('location', (location)=>{
+        io.emit('message', `https://google.com/maps?q=${location.latitude},${location.longitude}`);
+    })
+
+    socket.on('disconnect', ()=>{
+        io.emit('message', 'A user has left!');
+    });
+
 });
 
 server.listen(port, ()=>{
